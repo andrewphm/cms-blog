592f0500b8928f161a80788e4ea2ba2a2897d5c7
andrewphm <andrpham@gmail.com>
2021-11-16 02:20:55 -0500

    Add a header and layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
-import 'tailwindcss/tailwind.css';
+import React, { useEffect, useState } from 'react';
+
+// Layout
+import { Layout } from '../components';
 
 // Styles
+import 'tailwindcss/tailwind.css';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
